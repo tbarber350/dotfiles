6ec0d08d731d3cf117f7bb3887942a22951c375d
Travis Barber <travisbarber@Traviss-MacBook-Pro.local>
2023-12-09 14:22:43 -0700

    add more languages to installed for treesitter

--- nvim/.config/nvim/lua/travis/plugin/treesitter.lua
+++ nvim/.config/nvim/lua/travis/plugin/treesitter.lua
@@ -1,7 +1,18 @@
 local treesitter = require('nvim-treesitter.configs')
 
 treesitter.setup {
-    ensure_installed = {'python', 'vue', 'javascript', 'scss', 'json'},
+    ensure_installed = {
+       'c', 
+        'lua',
+        'vim',
+        'vimdoc',
+        'query',
+        'python',
+        'vue',
+        'javascript',
+        'scss',
+        'json'
+    },
     auto_install = true,
     highlight = {
         enable = true,
