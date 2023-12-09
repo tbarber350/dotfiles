1d5300ac29397d3327394df25f972230b3dc3f8e
Travis Barber <travisbarber@Traviss-MacBook-Pro.local>
2023-12-09 14:48:22 -0700

    uupdate theme in lualine

--- nvim/.config/nvim/lua/travis/plugin/lualine.lua
+++ nvim/.config/nvim/lua/travis/plugin/lualine.lua
@@ -3,7 +3,7 @@ if (not status) then return end
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'nightfox',
+    theme = 'kanagawa-wave',
     section_separators = {'', ''},
     component_separators = {'', ''},
     disabled_filetypes = {}
