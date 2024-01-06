9089e5792d9a806c86594c9310b6ccf6b7b70ea7
Travis Barber <travisbarber@Traviss-MacBook-Pro.local>
2024-01-06 07:07:19 -0700

    add new telescope mapping

--- nvim/.config/nvim/lua/travis/plugin/telescope.lua
+++ nvim/.config/nvim/lua/travis/plugin/telescope.lua
@@ -7,7 +7,7 @@ local opts = { noremap = true, silent = true }
 -- Shorten function name
 local keymap = vim.keymap.set
 
-telescope.setup{
+telescope.setup {
     defaults = {
         mappings = {
             i = {
@@ -24,17 +24,17 @@ telescope.load_extension('file_browser')
 
 local M = {}
 M.search_dotfiles = function()
-  local opts = {
+    local opts = {
         prompt_title = "< Dotfiles >",
         theme = 'ivy',
         cwd = '~/dotfiles/nvim/.config/nvim',
         hidden = true,
     }
-  require("telescope.builtin").find_files(themes.get_ivy(opts))
+    require("telescope.builtin").find_files(themes.get_ivy(opts))
 end
 
 M.curr_buff = function()
-  require('telescope.builtin').current_buffer_fuzzy_find(themes.get_ivy())
+    require('telescope.builtin').current_buffer_fuzzy_find(themes.get_ivy())
 end
 
 M.search_snippets = function()
@@ -48,29 +48,37 @@ end
 
 -- Find files using Telescope command-line sugar.
 keymap("n", "<Leader>ff", "<cmd>Telescope find_files theme=ivy<cr>", opts)
-keymap("n", "<Leader>fa", function() return require('telescope.builtin').find_files(themes.get_ivy({hidden=true})) end, opts)
+keymap("n", "<Leader>fa", function() return require('telescope.builtin').find_files(themes.get_ivy({ hidden = true })) end,
+    opts)
 keymap("n", "<Leader>fg", "<cmd>Telescope live_grep theme=ivy<cr>", opts)
 keymap("n", "<Leader>fb", "<cmd>Telescope buffers theme=ivy<cr>", opts)
 keymap("n", "<Leader>fh", "<cmd>Telescope help_tags theme=ivy<cr>", opts)
-keymap("n", "<Leader>fe", "<cmd>Telescope lsp_references theme=ivy<cr>", {noremap = true})
+keymap("n", "<Leader>fe", "<cmd>Telescope lsp_references theme=ivy<cr>", { noremap = true })
 keymap("n", "<Leader>ft", "<cmd>Telescope grep_string theme=ivy<cr>", opts)
-keymap("n", "<Leader>fd", function() return M.search_dotfiles() end, {noremap = true, silent = false})
-keymap("n", "<Leader>fs", function() return M.search_snippets() end, {noremap = true, silent = false})
+keymap("n", "<Leader>fd", function() return M.search_dotfiles() end, { noremap = true, silent = false })
+keymap("n", "<Leader>fs", function() return M.search_snippets() end, { noremap = true, silent = false })
 keymap("n", "<Leader>fc", function() return M.curr_buff() end, opts)
-keymap("n", "<Leader>lg", function() return require('telescope.builtin').live_grep({grep_open_files=true}) end, opts)
+keymap("n", "<Leader>lg", function() return require('telescope.builtin').live_grep({ grep_open_files = true }) end, opts)
 keymap("n", "<Leader>fr", function()
     require('telescope.builtin').grep_string({
         search = vim.fn.input("Grep For > ")
     })
 end, opts)
 
+keymap("n", "<Leader>fT", function()
+    local current_word = vim.fn.expand("<cWORD>")
+    local opts = {
+        search = current_word,
+    }
+    require('telescope.builtin').grep_string(themes.get_ivy(opts))
+end, opts)
 -- find the PascalCase version of the kebob-case word under the cursor
 -- useful for vue files at work
 keymap("n", "<Leader>fp", function()
     local current_word = vim.fn.expand("<cword>")
     -- change current_word from kebab-case to pascale case
-    current_word = current_word:gsub( '-(%a)', function(first)
-      return first:upper()
+    current_word = current_word:gsub('-(%a)', function(first)
+        return first:upper()
     end):gsub('^%a', function(first) return first:upper() end)
     local opts = {
         search = current_word,
@@ -81,8 +89,8 @@ end, opts)
 keymap("n", "<Leader>fi", function()
     local current_word = vim.fn.expand("<cword>")
     -- change current_word from kebab-case to pascale case
-    current_word = current_word:gsub( '^(%a)', function(first)
-      return '<' .. first:lower()
+    current_word = current_word:gsub('^(%a)', function(first)
+        return '<' .. first:lower()
     end):gsub('%u', function(first) return '-' .. first:lower() end)
     local opts = {
         search = current_word,
