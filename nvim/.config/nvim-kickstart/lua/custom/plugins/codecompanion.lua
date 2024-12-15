82f3f8ef9e00591ed07bf731bc32e921b4a1d15e
Travis Barber <=>
2024-12-15 11:04:21 -0700

    add codecompanion

---
+++ nvim/.config/nvim-kickstart/lua/custom/plugins/codecompanion.lua
@@ -0,0 +1,19 @@
+return {
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("codecompanion").setup({
+			strategies = {
+				chat = {
+					adapter = "copilot",
+				},
+				inline = {
+					adapter = "copilot",
+				},
+			},
+		})
+	end,
+}
